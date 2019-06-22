49664fb413e76cad1f6bebbc62ae20c5627833ca
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-06-21 23:11:08 -0600

    Create CLI guessing game
    Practice using #gets and a loop to create an interactive game

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -1,1 +1,18 @@
 # Code your solution here!
+def run_guessing_game
+  puts "Guess a number between 1 and 6."
+  number = rand(1..6)
+  user_response = gets.chomp
+  while user_response != number do
+    if user_response == "exit"
+      puts "Goodbye!"
+      break
+    else
+      puts "The computer guessed #{number}."
+      puts "Guess a number between 1 and 6."
+      number = rand(1..6)
+      user_response = gets.chomp
+    end
+  end
+  puts "You guessed the correct number!"
+end
